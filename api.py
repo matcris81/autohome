1b9d7b505df3b958fec866edcf1810c3c69d7438
Matei Crisea <nrs3007@autuni.ac.nz>
2024-02-25 19:03:57 +1300

    updated port and json

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import paho.mqtt.publish as publish
 import controller as controller
+import json
 
 app = Flask(__name__)
 MQTT_SERVER = "192.168.1.25"
@@ -11,24 +12,20 @@ MQTT_PASSWORD = '123456'
 @app.route('/control_ac', methods=['POST'])
 def control_ac():
     content = request.json
+    json_string = json.dumps(content)
     # command = content['command']
-    controller.encode_ac()  
     print(content)
     auth = {'username': MQTT_USERNAME, 'password': MQTT_PASSWORD}
-    publish.single(MQTT_PATH, content, hostname=MQTT_SERVER, auth=auth)
+    publish.single(MQTT_PATH, json_string, hostname=MQTT_SERVER, auth=auth)
     return jsonify({"status": "Command sent"}), 200
 
 @app.route('/garage', methods=['POST'])
 def control_garage():
     content = request.json
     command = content['command']
-    controller.encode_ac()  
-    print('garage')
-    print(content)
     auth = {'username': MQTT_USERNAME, 'password': MQTT_PASSWORD}
     publish.single(MQTT_PATH, command, hostname=MQTT_SERVER, auth=auth)
     return jsonify({"status": "Command sent"}), 200
 
-
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=5000)
+   app.run(host='0.0.0.0', port=6000)
