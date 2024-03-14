669a93fdbdde7e2645bb0c792a25b87585440d59
matcris81 <matcris81@gmail.com>
2024-03-15 10:23:26 +1300

    web socket connection to nest.js server

--- api.py
+++ api.py
@@ -2,12 +2,27 @@ from flask import Flask, request, jsonify
 import paho.mqtt.publish as publish
 import controller as controller
 import json
+import socketio
 
 app = Flask(__name__)
 MQTT_SERVER = "192.168.1.25"
 MQTT_PATH = "device/control"
 MQTT_USERNAME = 'matei'
 MQTT_PASSWORD = '123456'
+
+sio = socketio.Client()
+
+@sio.event
+def connect():
+    print('Connection established')
+    sio.emit('message', 'Pi is online')
+
+@sio.event
+def disconnect():
+    print('Disconnected from server')
+
+# sio.connect('https://your-nestjs-domain.com')
+sio.connect('http://192.168.1.7:3000')
 
 @app.route('/control_ac', methods=['POST'])
 def control_ac():
